91d7cdb824c34bda2558d31807b5a06a230c3884
BarnardF <168278143+BarnardF@users.noreply.github.com>
2025-02-24 16:12:17 +0000

    Update sources.js

--- module_4/definitions/sources.js
+++ module_4/definitions/sources.js
@@ -1,23 +1,23 @@
 declare({
-    database: "taxi-rides-ny-449710",
+    database: "project-id",
     schema: "trips_data_all",
     name: "green_tripdata"
 });
 
 declare({
-    database: "taxi-rides-ny-449710",
+    database: "project-id",
     schema: "trips_data_all",
     name: "yellow_tripdata"
 });
 
 declare({
-    database: "taxi-rides-ny-449710",
+    database: "project-id",
     schema: "trips_data_all",
     name: "fhv-data-2019"
 });
 
 declare({
-    database: "taxi-rides-ny-449710",
+    database: "project-id",
     schema: "trips_data_all",
     name: "taxi_zone_lookup"
 });
